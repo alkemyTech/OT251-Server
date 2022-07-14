1e19e8770d05bc5f5b7f3cedf79b74d00635aa17
Fabricio Barreto <Fabrib40@gmail.com>
2022-07-14 18:25:52 -0300

    Create model and repository

--- src/main/java/com/alkemy/ong/models/Roles.java
+++ src/main/java/com/alkemy/ong/models/Roles.java
@@ -1,11 +1,24 @@
 package com.alkemy.ong.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.sql.Timestamp;
 
+@Entity
+@Data
+@NoArgsConstructor
 public class Roles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @NonNull
     private String name;
@@ -14,5 +27,4 @@ public class Roles {
     private String descriptions;
 
     private Timestamp timestamp;
-
 }
