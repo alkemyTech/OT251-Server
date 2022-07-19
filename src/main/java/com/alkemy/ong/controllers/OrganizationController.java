6af58ebae54b45ed2247c9657b85b24aaf2a8270
SnMaxx <snmaxx@hotmail.com>
2022-07-18 22:13:02 -0300

    ONG251-23 fix Controller and MapStruct annotations

--- src/main/java/com/alkemy/ong/controllers/OrganizationController.java
+++ src/main/java/com/alkemy/ong/controllers/OrganizationController.java
@@ -5,13 +5,13 @@ import com.alkemy.ong.mappers.MapStruct;
 import com.alkemy.ong.models.OrganizationModel;
 import com.alkemy.ong.services.OrganizationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
-@Repository
+@RestController
 @RequestMapping("/organization")
 public class OrganizationController {
 
