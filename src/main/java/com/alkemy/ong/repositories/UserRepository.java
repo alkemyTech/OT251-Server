040376a2e73c6386eb0e5341f546c9c85a03b0ea
Erika Brito <erikajohanabrito@gmail.com>
2022-07-24 18:36:53 -0300

    se agrega loadUserbyEmail a UserServ

--- src/main/java/com/alkemy/ong/repositories/UserRepository.java
+++ src/main/java/com/alkemy/ong/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.alkemy.ong.repositories;
 
 import com.alkemy.ong.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -13,4 +14,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
 	public Boolean existsByEmail(String email);
 
+	//rama 25 Brito Erika
+	public UserDetails loadUserByEmail(String email);
 }
--- src/main/java/com/alkemy/ong/services/imp/UserServiceImp.java
+++ src/main/java/com/alkemy/ong/services/imp/UserServiceImp.java
@@ -1,7 +1,15 @@
 package com.alkemy.ong.services.imp;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.alkemy.ong.dto.request.user.UserRegisterRequest;
 import com.alkemy.ong.dto.response.user.UserResponse;
@@ -31,6 +39,29 @@ public class UserServiceImp implements IUserService {
 //		user.setRoles(Collections.singleton(roles));
 		return userMapper.mapResponse(userRepository.save(user));
 
+	}
+
+	//rama 25 Erika Brito
+	//@Override
+	@Transactional(readOnly = true)
+	public UserDetails loadUserByEmail(String email) throws Exception {
+		User usuario = userRepository.findByEmail(email);
+
+		if(usuario == null){
+			throw new Exception("No se encontró el email:" + email);		
+		}
+
+		boolean estado;
+		if (usuario.getDeleted()) //el parametro es enabled, traduzco desde deleted
+			estado = false;
+		else estado = true;
+
+		List<GrantedAuthority> authorities = usuario.getRoles()
+			.stream()
+			.map(Role -> new SimpleGrantedAuthority(Role.getName()))
+			.collect(Collectors.toList());
+
+		return new org.springframework.security.core.userdetails.User(usuario.getEmail(), usuario.getPassword(), estado, true, true, true, authorities);
 	}
 
 }
