1b2991ff96123d38aa416f9c67866dc11d83dbd3
SnMaxx <snmaxx@hotmail.com>
2022-07-29 18:03:05 -0300

    Revert "Feature/ong251 25"

--- src/main/java/com/alkemy/ong/services/imp/UserServiceImp.java
+++ src/main/java/com/alkemy/ong/services/imp/UserServiceImp.java
@@ -1,20 +1,11 @@
 package com.alkemy.ong.services.imp;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.alkemy.ong.dto.request.user.UserRegisterRequest;
 import com.alkemy.ong.dto.response.user.UserResponse;
 import com.alkemy.ong.exception.EmailAlreadyExistsException;
-import com.alkemy.ong.exception.EmailNotFoundException;
 import com.alkemy.ong.mappers.UserMapper;
 import com.alkemy.ong.models.User;
 import com.alkemy.ong.repositories.UserRepository;
@@ -40,34 +31,6 @@ public class UserServiceImp implements IUserService {
 //		user.setRoles(Collections.singleton(roles));
 		return userMapper.mapResponse(userRepository.save(user));
 
-	}
-
-	@Override
-	@Transactional(readOnly = true)
-	public User findByEmail(String email){
-		return userRepository.findByEmail(email);
-	}
-
-	//rama 25 Erika Brito
-	//@Override
-	@Transactional(readOnly = true)
-	public UserDetails loadUserByEmail(String email) throws EmailNotFoundException {
-		User usuario = userRepository.findByEmail(email);
-
-		if(usuario == null)
-			throw new EmailNotFoundException("No se encontró el email:" + email);		
-
-		boolean estado;
-		if (usuario.getDeleted()) //el parametro es enabled, traduzco desde deleted
-			estado = false;
-		else estado = true;
-
-		List<GrantedAuthority> authorities = usuario.getRoles()
-			.stream()
-			.map(Role -> new SimpleGrantedAuthority(Role.getName()))
-			.collect(Collectors.toList());
-
-		return new org.springframework.security.core.userdetails.User(usuario.getEmail(), usuario.getPassword(), estado, true, true, true, authorities);
 	}
 
 }
