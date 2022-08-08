8656656d7e23768ae4ea58f39bd25acf2b479710
Dayanna Zavarce <natali.zavarcer@gmail.com>
2022-08-08 11:04:35 -0300

    ONG251-56 Add Endpoint Slides Detail

--- src/main/java/com/alkemy/ong/services/ISlideService.java
+++ src/main/java/com/alkemy/ong/services/ISlideService.java
@@ -1,11 +1,15 @@
 package com.alkemy.ong.services;
 
 import java.util.List;
+import java.util.UUID;
 
 import com.alkemy.ong.dto.response.slides.SlideResponse;
+import com.alkemy.ong.dto.response.slides.SlidesDetailsResponse;
 
 public interface ISlideService {
 
 	List<SlideResponse>  getAll();
+
+	SlidesDetailsResponse getById(UUID id);
 
 }
