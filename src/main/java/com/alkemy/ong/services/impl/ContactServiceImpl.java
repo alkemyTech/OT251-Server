2b71200d120a84ad7f5ed2f647333285d215e4f2
Aaron Garay <aaronroman93@hotmail.com>
2022-08-09 15:57:40 -0300

    ONG251-61 Create endpoint (GET) /contacts

--- src/main/java/com/alkemy/ong/services/impl/ContactServiceImpl.java
+++ src/main/java/com/alkemy/ong/services/impl/ContactServiceImpl.java
@@ -2,12 +2,16 @@ package com.alkemy.ong.services.impl;
 
 import com.alkemy.ong.dto.request.contact.ContactRequest;
 import com.alkemy.ong.dto.response.contact.ContactResponse;
+import com.alkemy.ong.exception.ResourceNotFoundException;
 import com.alkemy.ong.mappers.ContactMapper;
 import com.alkemy.ong.models.Contact;
 import com.alkemy.ong.repositories.ContactRepository;
 import com.alkemy.ong.services.IContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ContactServiceImpl implements IContactService {
@@ -22,5 +26,14 @@ public class ContactServiceImpl implements IContactService {
     public ContactResponse save(ContactRequest contactRequest) {
         Contact contact = contactMapper.contactRequestToEntity(contactRequest);
         return contactMapper.mapContactResponse(contactRepo.save(contact));
+    }
+
+    @Override
+    public List<ContactResponse> getAll(){
+        List<Contact> contacts = contactRepo.findAll();
+        if(contacts.isEmpty()) {
+            throw new ResourceNotFoundException("List contacts");
+        }
+        return contacts.stream().map(contact -> contactMapper.mapContactResponse(contact)).collect(Collectors.toList());
     }
 }
