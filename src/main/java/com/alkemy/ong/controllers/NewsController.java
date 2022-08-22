cf310e184ec05bf40832610f6d7e1531b997304f
SnMaxx <snmaxx@hotmail.com>
2022-08-22 17:33:04 -0300

    Add documentation to News

--- src/main/java/com/alkemy/ong/controllers/NewsController.java
+++ src/main/java/com/alkemy/ong/controllers/NewsController.java
@@ -5,6 +5,9 @@ import java.util.UUID;
 
 import javax.validation.Valid;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -36,19 +39,31 @@ public class NewsController {
 
 	@Autowired
 	private ICommentService commentService;
-	
+
+	@ApiOperation(value = "Method to create a News", notes = "Save a new News item in the database and return it")
+	@ApiResponses(value = {
+			@ApiResponse(code = 201, message = "CREATED - News create successfully"),
+			@ApiResponse(code = 400, message = "BAD REQUEST - Param invalid")})
 	@PreAuthorize("hasRole('ADMIN')")
 	@PostMapping
 	public ResponseEntity<NewsResponse> createNews(@RequestBody @Valid NewsRequest newsRequest) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(newsServices.createNews(newsRequest));
 	}
 
+	@ApiOperation(value = "Method to get a News by ID", notes = "Return News details if the ID exist")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "OK - Return News details"),
+			@ApiResponse(code = 404, message = "NOT FOUND - A News with that ID is Not Found")})
 	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/{id}")
 	public ResponseEntity<NewsResponse> getNewsById(@PathVariable UUID id) {
 		return ResponseEntity.status(HttpStatus.OK).body(newsServices.getById(id));
 	}
 
+	@ApiOperation(value = "Method to delete a News by ID", notes = "Delete a News if the ID exist")
+	@ApiResponses(value = {
+			@ApiResponse(code = 204, message = "NO CONTENT - News delete successfully"),
+			@ApiResponse(code = 404, message = "NOT FOUND - A News with that ID is Not Found")})
 	@PreAuthorize("hasRole('ADMIN')")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> deleteNews(@PathVariable UUID id) {
@@ -56,11 +71,20 @@ public class NewsController {
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 
+	@ApiOperation(value = "Method to update a News by ID", notes = "Update a News if the ID exist")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "OK - News update successfully"),
+			@ApiResponse(code = 400, message = "BAD REQUEST - Param invalid"),
+			@ApiResponse(code = 404, message = "NOT FOUND - A News with that ID is Not Found")})
 	@PutMapping("/{id}")
 	public ResponseEntity<NewsResponse> updateNews(@PathVariable UUID id, @RequestBody @Valid NewsRequest newsRequest) {
 		return ResponseEntity.status(HttpStatus.OK).body(newsServices.update(id, newsRequest));
 	}
 
+	@ApiOperation(value = "Method to Get the comments for a News", notes = "Get the comments for a News if the ID(News) exist")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "OK - Return comments"),
+			@ApiResponse(code = 404, message = "NOT FOUND - A News with that ID is Not Found")})
 	@GetMapping("/{id}/comments")
 	public ResponseEntity<Page<CommentListResponse>> listCommentsByNewsId(@PageableDefault(size = 10) Pageable pageable,@PathVariable(value = "id") UUID id) {
 		return ResponseEntity.ok(commentService.getCommentsByNewsId(id, pageable));
